f80a114d56139a5df8809c81c613bd9aaf704b27
JamesB <James.buchmann@gmail.com>
2022-06-30 10:05:21 -0500

    fixing the issue that was messing with the questions and answers

--- assests/script/script.js
+++ assests/script/script.js
@@ -1,12 +1,15 @@
 var currentQuestion = 0;
+var points = 0;
+var sec = 59;
 
 
 function timer(){
-    var sec = 59;
     var timer = setInterval(function(){
+        document.getElementById('scoreDisplay').innerHTML=points;
         document.getElementById('safeTimerDisplay').innerHTML=sec;
         sec--;
         if (sec < 0) {
+            currentQuestion =-1
             clearInterval(timer);
         }
     }, 1000);
@@ -14,8 +17,10 @@ function timer(){
 function startQuiz() {
     document.getElementById('start_btn').style.display = "none";
     document.getElementById('quiz').style.display = "block";
+    document.getElementById('scoreDisplay').style.display = "inline";
     nextQuestion(); 
 }
+
 function nextQuestion() {
     var quizDiv = document.getElementById('quiz');
     var i = getCurrentQuestion(); 
@@ -30,21 +35,34 @@ function nextQuestion() {
     newAnswer2.innerHTML = questions[i].answer2;
     newAnswer3.innerHTML = questions[i].answer3;
     newAnswer4.innerHTML = questions[i].answer4;
-
-    setCurrentQuestion();
 }
 function getCurrentQuestion() {
     return currentQuestion;
 
 }
 function setCurrentQuestion() {
-    if(currentQuestion < questions.length) {
+    if(currentQuestion < questions.length && currentQuestion >= 0) {
         currentQuestion++;
     }
+    else{
+        currentQuestion = -1;
+    }
 }
-function checkAnswer() {
+function checkAnswer(answerId) {
+    
+
     var i = getCurrentQuestion();
-    if(questions[i].correctAnswer == )
+    console.log("current question "  + i )
+    if(questions[i].correctAnswer == answerId) {
+        points++;
+        console.log('correctAnswer ' + answerId);
+    }
+    else {
+        sec-=5;
+        console.log("wrong " + answerId);
+    }
+    setCurrentQuestion();
+    nextQuestion();
 }
 
 // for (var i = 0; i < questions.length; i++) {
@@ -69,15 +87,15 @@ var questions = [
         answer3: 'javascript',
         answer4: 'scripting',
         question: 'Inside which HTML element do we put the JavaScript file?',
-        correctAnswer: 1
+        correctAnswer: 'answer1btn'
     },
     {
-        answer1: 'alert("Hell)o World")',
+        answer1: 'alert("Hello World")',
         answer2: 'msg("Hello World");',
         answer3: 'alert("Hello World");',
         answer4: 'printf("Hello World");',
         question: 'How do you write "Hello World" in an alert box?',
-        correctAnswer: 3
+        correctAnswer: 'answer3btn'
     },
     {
         answer1: 'if i = 5',
@@ -85,7 +103,7 @@ var questions = [
         answer3: 'if i == 5 then',
         answer4: 'if(i==5)',
         question: 'How to write an IF statement in JavaScript?',
-        correctAnswer: 4
+        correctAnswer: 'answer4btn'
     },
 
 ]
